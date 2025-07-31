c2cc41636000588205919b2fca0e0120cd29ebe4
kunal <monupanwar5655@gmail.com>
2025-07-31 10:30:51 +0530

    refactoring

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,11 @@
-import { PrismaClient } from "./generated/prisma";
-
+// lib/prisma.ts
+import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
+  prisma: PrismaClient | undefined
 }
+
+export const prisma =
+  globalForPrisma.prisma ?? new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
